c47fbf59bd9841d3855f7c37ed63b319472fa661
BlueSaber <github@dylanr.net>
2022-08-31 00:18:25 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BlueSaberDocs'
+copyright = '2022, BlueSaber'
+author = 'BlueSaber'
 
 release = '0.1'
 version = '0.1.0'
